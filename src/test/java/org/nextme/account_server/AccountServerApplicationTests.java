3b99fe65fe8ae6316f78d5ceb4192bcdf6e0343a
vsuminv <tnalsdl5075@naver.com>
2025-12-03 13:03:02 +0900

    add : 프로젝트 설정

---
+++ src/test/java/org/nextme/account_server/AccountServerApplicationTests.java
@@ -0,0 +1,13 @@
+package org.nextme.account_server;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AccountServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
