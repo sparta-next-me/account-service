65408bbacb65ebd4d424746a1d9c6c0904da0deb
vsuminv <tnalsdl5075@naver.com>
2025-12-03 13:57:05 +0900

    refactor : #1 엔티티 필드 주석 생성

--- src/main/java/org/nextme/account_server/account/domain/entiry/Tran/Tran.java
+++ src/main/java/org/nextme/account_server/account/domain/entiry/Tran/Tran.java
@@ -18,29 +18,37 @@ import java.util.UUID;
 public class Tran {
 
     @EmbeddedId
+    // 거래ID
     private TranId tranId;
 
     @JoinColumn(name = "account_id", nullable = false)
     @ManyToOne(fetch = FetchType.LAZY)
+    // 계좌ID
     private Account account;
 
     @Column(name = "user_id", nullable = false)
+    // 유저ID
     private UUID userId;
 
     @Column(name = "tran_date", nullable = false)
+    // 거래시간
     private LocalDateTime tranDate;
 
     @Column(name = "withdraw")
+    // 출금금액
     private int withDraw;
 
     @Column(name = "deposit")
+    // 입금금액
     private int deposit;
 
     @Column(name = "current_balance", nullable = false)
+    // 현재잔액
     private int currentBalance;
 
     @Column(name = "tran_type", nullable = false)
     @Enumerated(EnumType.STRING)
+    // 거래구분
     private TranType tranType;
 
 }
