92547d09519c7571737019befc54aebc7381154c
vsuminv <tnalsdl5075@naver.com>
2025-12-12 18:03:49 +0900

    feat : 배포 설정

--- src/test/java/org/nextme/account_server/AccountServerApplicationTests.java
+++ src/test/java/org/nextme/account_server/AccountServerApplicationTests.java
@@ -12,6 +12,9 @@ class AccountServerApplicationTests {
 
 	@Test
 	void contextLoads() {
+        String a = "%7B%22result%22%3A%7B%22code%22%3A%22CF-00000%22%2C%22extraMessage%22%3A%22%22%2C%22message%22%3A%22%EC%84%B1%EA%B3%B5%22%2C%22transactionId%22%3A%22693bab61231b0a42452dcbe5%22%7D%2C%22data%22%3A%7B%22successList%22%3A%5B%7B%22clientType%22%3A%22P%22%2C%22code%22%3A%22CF-00000%22%2C%22loginType%22%3A%221%22%2C%22countryCode%22%3A%22KR%22%2C%22organization%22%3A%220088%22%2C%22extraMessage%22%3A%22%22%2C%22businessType%22%3A%22BK%22%2C%22message%22%3A%22%EC%84%B1%EA%B3%B5%22%2C%22transactionId%22%3A%22693bab61231b0a42452dcbe6%22%7D%5D%2C%22errorList%22%3A%5B%5D%2C%22connectedId%22%3A%228CRaJ1whQJDapTaF2iD3si%22%7D%7D";
+        String b = URLDecoder.decode(a, StandardCharsets.UTF_8);
+        System.out.println(b);
     }
 
 }
