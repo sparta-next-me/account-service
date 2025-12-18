8f99bdefe1b1580781c598618c9e04e9166b8f2b
vsuminv <tnalsdl5075@naver.com>
2025-12-18 17:23:25 +0900

    bugfix : 계정 삭제 오류

--- src/main/java/org/nextme/account_server/account/infrastructure/presentation/dto/request/AccountDeleteRequest.java
+++ src/main/java/org/nextme/account_server/account/infrastructure/presentation/dto/request/AccountDeleteRequest.java
@@ -1,5 +1,6 @@
 package org.nextme.account_server.account.infrastructure.presentation.dto.request;
 
+import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import org.jetbrains.annotations.NotNull;
@@ -8,11 +9,12 @@ import java.util.List;
 import java.util.UUID;
 
 @Builder
-public record AccountDeleteRequest(
-        //@NotNull List<AccountList> accountList, // 사용자 계정 정보 리스트
-        @NotNull String connectedId, // 커넥티드 아이디
-        @NotNull UUID accountId,//계좌 아이디
-        UUID userId
-) {
+@Getter
+@AllArgsConstructor
+public class AccountDeleteRequest {
+    //@NotNull List<AccountList> accountList, // 사용자 계정 정보 리스트
+    @NotNull String connectedId;// 커넥티드 아이디
+    @NotNull UUID accountId;//계좌 아이디
+    UUID userId;
 
 }
